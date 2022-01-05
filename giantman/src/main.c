8a43f978f6e23c62731c5d0ba0b126e2fd307380
Saverio <xavier.mitault@epitech.eu>
2022-01-05 19:41:24 +0100

    [update] .gitignore: allow push antman and giantman folder

---
+++ giantman/src/main.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** GIANTMAN
+** File description:
+** main function
+*/
+
+#include <stdlib.h>
+#include "my_fs.h"
+#include "my_puts.h"
+
+int main(int ac, char **av)
+{
+    char *buff;
+
+    if (ac < 2)
+        return (84);
+    buff = fs_get_content(av[1]);
+    if (buff == NULL)
+        return (84);
+    my_putstr(buff);
+    free(buff);
+    return (0);
+}
