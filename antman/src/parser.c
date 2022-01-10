256d0cf848922089b0e514b0e04296b7cc62f8b5
simon.vermeulen <simon.vermeulen@epitech.eu>
2022-01-10 17:21:17 +0100

    [update]: parser handling multiple spaces

--- antman/src/parser.c
+++ antman/src/parser.c
@@ -11,9 +11,17 @@
 
 static void get_word_value(char *word, char *buff, int word_len) {
 
-    for (int i = 0; buff[i] && buff[i] != ' '; i++)
+    for (int i = 0; buff[i] && i < word_len; i++)
         word[i] = buff[i];
     word[word_len] = '\0';
+}
+
+static int get_nb_spaces(char *buffer)
+{
+    int nb_spaces = 0;
+
+    for (; buffer[nb_spaces] && buffer[nb_spaces] == ' '; nb_spaces++);
+    return (nb_spaces);
 }
 
 list_t *launch_parsing(char *buffer)
@@ -21,13 +29,21 @@ list_t *launch_parsing(char *buffer)
     int word_len = 0;
     char *word = NULL;
     list_t *list = NULL;
+    int nb_spaces = 0;
 
     for (int i = 0; buffer[i]; i++) {
-        if (buffer[i] == ' ' || !buffer[i + 1]) {
+        nb_spaces = get_nb_spaces(&buffer[i]);
+        if (nb_spaces || !buffer[i + 1]) {
             word = malloc(sizeof(char) * (word_len + 1));
             get_word_value(word, &buffer[i - word_len], word_len);
             list = list_t_add(list, word);
             word_len = 0;
+            if (nb_spaces > 1) {
+                word = malloc(sizeof(char) * (nb_spaces + 1));
+                get_word_value(word, &buffer[i], nb_spaces);
+                list = list_t_add(list, word);
+                i += nb_spaces - 1;
+            }
         } else {
             word_len++;
         }
