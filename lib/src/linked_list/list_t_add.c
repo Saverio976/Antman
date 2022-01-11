c33cf26a2008e9a1b7ff3f75b3e64388cea06a3a
Saverio <xavier.mitault@epitech.eu>
2022-01-11 19:31:20 +0100

    [add] lib/src/linked_list/list_t_add.c: add only if the list not contains the data

--- lib/src/linked_list/list_t_add.c
+++ lib/src/linked_list/list_t_add.c
@@ -8,25 +8,36 @@
 #include <stdlib.h>
 #include "linked_list.h"
 
-list_t *list_t_add(list_t *list, char *data)
+static void update_link_list(list_t *list, list_t *new)
 {
-    list_t *new = malloc(sizeof(list_t));
-
-    if (new == NULL) {
-        return (NULL);
-    }
-    new->word = data;
-    if (list == NULL) {
-        new->prev = new;
-        new->next = new;
-        new->index = 0;
-        return (new);
-    }
     new->prev = list->prev;
     new->next = list;
     new->index = list->prev->index + 1;
     new->frequency = 0;
     list->prev->next = new;
     list->prev = new;
+}
+
+list_t *list_t_add(list_t *list, char *data)
+{
+    list_t *new;
+    list_t *tmp;
+
+    if (list == NULL) {
+        new = malloc(sizeof(list_t));
+        new->word = data;
+        new->prev = new;
+        new->next = new;
+        new->index = 0;
+        return (new);
+    }
+    tmp = list_t_contains(list, data);
+    if (tmp != NULL) {
+        tmp->frequency += 1;
+    } else {
+        new = malloc(sizeof(list_t));
+        new->word = data;
+        update_link_list(list, new);
+    }
     return (list);
 }
