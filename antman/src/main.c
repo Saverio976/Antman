471258db323667ef5e5d3f4984629a7b41199846
Saverio <xavier.mitault@epitech.eu>
2022-01-13 14:42:46 +0100

    [add] antman/src/main.c antman/src/get_list_from_file.c: call some method to ge frequency and binary tree

--- antman/src/main.c
+++ antman/src/main.c
@@ -8,11 +8,28 @@
 #include <stdlib.h>
 #include "project.h"
 #include "my_fs.h"
+#include "linked_list.h"
+#include "binary_tree.h"
 #include "my_puts.h"
+#include "antman.h"
+
+static binary_tree_t *convert_list_to_binary(list_t *list)
+{
+    binary_tree_t *tree = binary_tree_t_create();
+    list_t *tmp;
+
+    while (list != NULL) {
+        tmp = list_t_pop_less_frequency(&list);
+        binary_tree_t_push_huffman(tree, tmp->word);
+    }
+    return (tree);
+}
 
 int starter_main(char const *file, int type)
 {
     fs_content_t *buff;
+    list_t *list;
+    binary_tree_t *tree;
 
     my_putnbr(type);
     if (type != 1 && type != 2 && type != 3) {
@@ -21,8 +38,8 @@ int starter_main(char const *file, int type)
     buff = fs_get_content(file);
     if (buff == NULL)
         return (84);
-    my_putstr(buff->content);
-    free(buff->content);
-    free(buff);
+    list = get_list_from_buff(buff->content, buff->size);
+    tree = convert_list_to_binary(list);
+    fs_content_t_free(buff);
     return (0);
 }
