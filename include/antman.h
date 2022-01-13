0f76d406a73851dc2fbe1eabeaf903805cb01b22
Saverio <xavier.mitault@epitech.eu>
2022-01-13 14:55:49 +0100

    [add] include/antman.h: add header for the antman part

---
+++ include/antman.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** ANTMAN
+** File description:
+** antman
+*/
+
+#ifndef ANTMAN_H_
+    #define ANTMAN_H_
+
+    #include "linked_list.h"
+
+list_t *get_list_from_buff(char *, int);
+
+#endif
