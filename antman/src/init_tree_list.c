5c81e56ab0dd3937b0a2e709acaa52bf8e2bdf0e
Saverio <xavier.mitault@epitech.eu>
2022-01-14 01:13:49 +0100

    [add] antman/src/init_tree_list.c: mean to create the binary tree from the text

---
+++ antman/src/init_tree_list.c
@@ -0,0 +1,65 @@
+/*
+** EPITECH PROJECT, 2022
+** ANTMAN
+** File description:
+** int tree and the correpodant list
+*/
+
+#include <stddef.h>
+#include "my_fs.h"
+#include "binary_tree.h"
+#include "linked_list.h"
+
+static void fill_node_list(node_t **node, list_t **list, fs_content_t *cont)
+{
+    node_t *tmp;
+
+    *node = NULL;
+    *list = NULL;
+    for (int i = 0; i < 255; i++) {
+        *list = list_t_add(*list, 0);
+    }
+    for (int i = 0; i < cont->size; i++) {
+        tmp = node_t_as_list_contains(*node, cont->content[i]);
+        if (tmp == NULL) {
+            *node = node_t_as_list_add_char(*node, cont->content[i]);
+        } else {
+            tmp->fat = tmp->fat + 1;
+        }
+    }
+}
+
+static void update_recussiv_path(node_t *node, int pad, list_t *list)
+{
+    list_t *elem;
+
+    if (node->is_child == 0 && node->left != NULL) {
+        update_recussiv_path(node->left, pad, list);
+    }
+    if (node->is_child == 0 && node->right != NULL) {
+        update_recussiv_path(node->right, pad, list);
+    }
+    if (node->is_child == 1) {
+        elem = list_t_get_index(list, node->c);
+        elem->path = elem->path << 1;
+        elem->path += pad;
+        elem->nb_bit += 1;
+    }
+}
+
+void init_tree_list(node_t **node, list_t **list, fs_content_t *cont)
+{
+    node_t *node_left;
+    node_t *node_right;
+    node_t *tmp;
+
+    fill_node_list(node, list, cont);
+    for (int i = 0; i < cont->size; i++) {
+        node_right = node_t_as_list_pop_less(node);
+        update_recussiv_path(node_right, 1, *list);
+        node_left = node_t_as_list_pop_less(node);
+        update_recussiv_path(node_left, 0, *list);
+        tmp = node_t_as_node_create_from(node_left, node_right);
+        *node = node_t_as_list_add_node(*node, tmp);
+    }
+}
